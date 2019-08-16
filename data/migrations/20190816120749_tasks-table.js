db4485e829cb31a2700985e21db60204cd36591f
Katie Rose <katiefelten@gmail.com>
2019-08-16 12:14:30 -0400

    :zap: created tasks table

---
+++ data/migrations/20190816120749_tasks-table.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("tasks", tbl => {
+    tbl.increments();
+    tbl.string("name", 255).notNullable();
+    tbl.string("description", 255).notNullable();
+    tbl.string("notes", 255).notNullable();
+    tbl.boolean("completed").defaultTo(false);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("tasks");
+};
