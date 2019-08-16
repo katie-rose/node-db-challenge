c60aa2510aa4a4622bb9c8c9899baf8db465b481
Katie Rose <katiefelten@gmail.com>
2019-08-16 12:45:37 -0400

    :zap: added models

---
+++ data/models/tasksModel.js
@@ -0,0 +1,24 @@
+const db = require("../dataConfig");
+
+module.exports = {
+  add,
+  find,
+  findById
+};
+
+async function find() {
+  return await db("tasks");
+}
+
+async function findById(project_id) {
+  return (
+    (await db("tasks")
+      .where({ project_id })
+      .first()) || null
+  );
+}
+
+async function add(project) {
+  const id = await db("tasks").insert(project, "id");
+  return findById(...id);
+}
