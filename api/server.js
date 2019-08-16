07471bccc9bc1dea5550a87600be2e119b35a3f2
Katie Rose <katiefelten@gmail.com>
2019-08-16 12:02:52 -0400

    :zap: setup routers with a 200 res on each

--- api/server.js
+++ api/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const server = express();
 
-const projectsRouter = require("./routers/projectRouter");
-const tasksRouter = require("./routers/taskRouter");
-
-const server = express();
+const projectsRouter = require("../routers/projectsRouter");
+const tasksRouter = require("../routers/tasksRouter");
+const resourcesRouter = require("../routers/resourcesRouter");
 
 server.use(express.json());
 
 server.use("/api/projects", projectsRouter);
 server.use("/api/tasks", tasksRouter);
+server.use("/api/resources", resourcesRouter);
 
 module.exports = server;
